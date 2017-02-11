3d0fe68250eac70a788eac9f4660027b5e6597d0
Ben Sterenson <bensterenson@gmail.com>
2017-02-11 21:01:56 +0200

    xamarin BLE Project

---
+++ PlantIO/PlantIOPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using Xamarin.Forms;
+using Plugin.BLE.Abstractions.Contracts;
+using Plugin.BLE.Abstractions.EventArgs;
+using Plugin.BLE.Abstractions.Utils;
+using Plugin.BLE.Abstractions.Extensions;
+using Plugin.BLE.Abstractions.Exceptions;
+using Plugin.BLE;
+using System.Diagnostics;
+using System;
+
+
+namespace PlantIO
+{
+	public partial class PlantIOPage : ContentPage
+	{
+		//public IDevice device;
+
+		public PlantIOPage()
+		{
+			InitializeComponent();
+		}
+
+		//IBluetoothLE ble = CrossBluetoothLE.Current;
+		IAdapter adapter = CrossBluetoothLE.Current.Adapter;
+
+		//ble.StateChanged += (s, c) => {Debug.WriteLine($"The bluetooth state changed to {c.NewState}");};
+
+		//adapter.DeviceDiscovered += (s, a) =>{Debug.WriteLine($"Device found: {a.Device.Name}");};
+
+		//adapter.StartScanningForDevicesAsync();
+
+		public void OnButtonClicked(object sender, EventArgs e)
+		{
+			//adapter.StartScanningForDevicesAsync();
+			var device = adapter.DiscoverDeviceAsync(dev => dev.Name.Equals("Project Zero"));
+			devName.Text = device.ToString();
+		}
+
+	}
+}
