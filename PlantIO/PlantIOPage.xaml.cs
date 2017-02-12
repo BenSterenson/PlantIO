012cb23a0f8d85cab089a7c3c52b262b697661c6
Ben Sterenson <bensterenson@gmail.com>
2017-02-12 02:38:32 +0200

    scans for ble

--- PlantIO/PlantIOPage.xaml.cs
+++ PlantIO/PlantIOPage.xaml.cs
@@ -8,12 +8,16 @@ using Plugin.BLE;
 using System.Diagnostics;
 using System;
 using System.Collections.ObjectModel;
+using System.Linq;
 
 namespace PlantIO
 {
 	public partial class PlantIOPage : ContentPage
 	{
-		public IDevice device;
+		public IDevice currDevice;
+		public IBluetoothLE _bluetooth = CrossBluetoothLE.Current;
+		public IAdapter _bleAdapter = CrossBluetoothLE.Current.Adapter;
+		public ObservableCollection<IDevice> _bleDevices = new ObservableCollection<IDevice> {};
 
 		public PlantIOPage()
 		{
@@ -24,7 +28,6 @@ namespace PlantIO
 				if (blePicker.SelectedIndex == -1)
 				{
 					devName.Text = "no Device selected";
-					device = null;
 				}
 				else
 				{
@@ -33,10 +36,10 @@ namespace PlantIO
 				}
 			};
 
+			_bleAdapter.DeviceDiscovered += bleDiscovered;
+
 		}
 
-		//IBluetoothLE ble = CrossBluetoothLE.Current;
-		IAdapter adapter = CrossBluetoothLE.Current.Adapter;
 
 		//ble.StateChanged += (s, c) => {Debug.WriteLine($"The bluetooth state changed to {c.NewState}");};
 
@@ -44,28 +47,42 @@ namespace PlantIO
 
 		//adapter.StartScanningForDevicesAsync();
 
+		public async void bleDiscovered(object sender, DeviceEventArgs e)
+		{
+			currDevice = e.Device;
+			//Grep the first device only.  
+			if (_bleAdapter.IsScanning)
+				await _bleAdapter.StopScanningForDevicesAsync();
+			
+			if (!_bleDevices.Any(dev => dev.Id == currDevice.Id))
+			{
+				_bleDevices.Add(currDevice);
+				this.blePicker.Items.Clear();
+
+				foreach (var dev in _bleDevices)
+				{
+					this.blePicker.Items.Add(dev.Name);
+				}
+			}
+
+		}
+
+
 		public void OnButtonClicked(object sender, EventArgs e)
 		{
-			adapter.StartScanningForDevicesAsync();
+			_bleAdapter.StartScanningForDevicesAsync();
 			//var mylist = adapter.DiscoveredDevices;
 			//var device = adapter.DiscoverDeviceAsync(dev => dev.Name.Equals("Project Zero"));
 			//devName.Text = device.ToString();
-
-			var ListViewItems = new ObservableCollection<string> { "one", "two", "three" };
-
-			this.blePicker.Items.Clear();
-
-			foreach (var num in ListViewItems)
-			{
-				blePicker.Items.Add(num);
-			}
 		}
+
 
 		public void OnButtonClickedConnect(object sender, EventArgs e)
 		{
-			adapter.ConnectToDeviceAsync(device);
+			_bleAdapter.ConnectToDeviceAsync(currDevice);
 
 		}
 
 	}
+
 }
